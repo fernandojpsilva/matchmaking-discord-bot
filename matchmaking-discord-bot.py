41fe6037362a4b01109aa4011e23d3cbda0a1216
fernando.j.silva <fjo.psilva@gmail.com>
2022-04-29 21:45:46 +0100

    get in game data complete

--- matchmaking-discord-bot.py
+++ matchmaking-discord-bot.py
@@ -1,5 +1,6 @@
 import discord
 import cassiopeia as cass
+from table2ascii import table2ascii as t2a, PresetStyle
 
 TOKEN = ''
 cass.set_riot_api_key('')
@@ -12,8 +13,10 @@ def getBlueTeam(match):
 
     for item in match.blue_team.participants:
         summoner_name = item.summoner.name
+        champion = item.champion.name
         rank = getSummonerRank(summoner_name)
-        blue_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points])
+        wr = str(round(rank.wins / (rank.wins + rank.losses) * 100, 2))
+        blue_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points, wr, champion])
 
     return blue_summoners_list
 
@@ -23,19 +26,34 @@ def getRedTeam(match):
 
     for item in match.red_team.participants:
         summoner_name = item.summoner.name
+        champion = item.champion.name
         rank = getSummonerRank(summoner_name)
-        red_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points])
+        wr = str(round(rank.wins / (rank.wins + rank.losses) * 100, 2))
+        red_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points, wr, champion])
 
     return red_summoners_list
 
 
 def generateMatchString(summoner_name):
-    match = getCurrentMatch(summoner_name)
-    blue_summoners_list = getBlueTeam(match)
-    red_summoners_list = getRedTeam(match)
-    print(blue_summoners_list)
-    print(red_summoners_list)
-    return blue_summoners_list
+    try:
+        match = getCurrentMatch(summoner_name)
+        blue_summoners_list = getBlueTeam(match)
+        red_summoners_list = getRedTeam(match)
+        send_list = []
+        for i in range(5):
+            send_list.append([blue_summoners_list[i][0], f'{blue_summoners_list[i][5]}', f'{blue_summoners_list[i][1]} {blue_summoners_list[i][2]}', f'{blue_summoners_list[i][4]}%',
+                              "x", f'{red_summoners_list[i][4]}%', f'{red_summoners_list[i][1]} {red_summoners_list[i][2]}', f'{red_summoners_list[i][5]}', red_summoners_list[i][0]])
+        output = t2a(
+            header=["Blue Side", "Champion", "Rank", "WR", "x", "WR", "Rank", "Champion", "Red Side"],
+            body=send_list,
+            style=PresetStyle.thin_compact
+        )
+    except Exception as err:
+        err_string = "Esse bro não me parece tar a jogar :glean:"
+        return err_string
+
+    out_string = f"```\n{output}\n```"
+    return out_string
 
 
 def getCurrentMatch(summoner_name):
@@ -53,19 +71,21 @@ def generateSummonerString(summoner_name):
     summoner_rank = getSummonerRank(summoner_name)
 
     if summoner is not False:
-        if summoner_rank is not False:
+        if summoner_rank != "Unranked":
             wr = str(round(summoner_rank.wins/(summoner_rank.wins+summoner_rank.losses)*100, 2))
             played = summoner_rank.wins+summoner_rank.losses
             embed = discord.Embed(title=f'{summoner.name} - Nível {summoner.level}',
                                   description=f'**{summoner_rank.league.tier} {summoner_rank.division} '
                                               f'({summoner_rank.league_points} LP) - {wr}% WR ({played} Jogos)**\n\n'
-                                              f'**[OPGG](https://euw.op.gg/summoners/euw/{summoner_name})**',
+                                              f'**[OPGG](https://euw.op.gg/summoners/euw/'
+                                              f'{summoner_name.replace(" ", "")})**',
                                   colour=discord.Colour.blue())
             return embed
         else:
             embed = discord.Embed(title=f'{summoner.name} - Nível {summoner.level}',
-                                  description=f'Unranked\n\n'
-                                              f'**[OPGG](https://euw.op.gg/summoners/euw/{summoner_name})**',
+                                  description=f'**Unranked**\n\n'
+                                              f'**[OPGG](https://euw.op.gg/summoners/euw/'
+                                              f'{summoner_name.replace(" ", "")})**',
                                   colour=discord.Colour.blue())
             return embed
     else:
@@ -95,7 +115,8 @@ def getSummonerRank(name):
         rank = cass.get_summoner(name=name, region='EUW').league_entries.fives
         return rank
     except Exception as err:
-        return False
+        unranked = "Unranked"
+        return unranked
 
 
 def getSummoner(name):
