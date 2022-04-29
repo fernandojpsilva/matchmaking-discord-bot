7b686bd32d1a85aae5d1407eb01f066c10b93f98
fernando.j.silva <fjo.psilva@gmail.com>
2022-04-29 15:20:23 +0100

    summoner cards, current match wip

--- matchmaking-discord-bot.py
+++ matchmaking-discord-bot.py
@@ -7,39 +7,92 @@ cass.set_riot_api_key('')
 client = discord.Client()
 
 
-def getSummonerString(user_message):
-    # Split game from summoner name, strip summoner name for
-    # names with multiple words
-    split_message = user_message.split(" ", 1)
-    name = split_message[1].strip()
-    summoner = getSummoner(name)
-    summoner_rank = getSummonerRank(name)
+def getBlueTeam(match):
+    blue_summoners_list = []
+
+    for item in match.blue_team.participants:
+        summoner_name = item.summoner.name
+        rank = getSummonerRank(summoner_name)
+        blue_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points])
+
+    return blue_summoners_list
+
+
+def getRedTeam(match):
+    red_summoners_list = []
+
+    for item in match.red_team.participants:
+        summoner_name = item.summoner.name
+        rank = getSummonerRank(summoner_name)
+        red_summoners_list.append([summoner_name, rank.league.tier, rank.division, rank.league_points])
+
+    return red_summoners_list
+
+
+def generateMatchString(summoner_name):
+    match = getCurrentMatch(summoner_name)
+    blue_summoners_list = getBlueTeam(match)
+    red_summoners_list = getRedTeam(match)
+    print(blue_summoners_list)
+    print(red_summoners_list)
+    return blue_summoners_list
+
+
+def getCurrentMatch(summoner_name):
+    summoner = getSummoner(summoner_name)
+    try:
+        match = summoner.current_match
+        return match
+    except Exception as err:
+        error = 'Not in game'
+        return error
+
+
+def generateSummonerString(summoner_name):
+    summoner = getSummoner(summoner_name)
+    summoner_rank = getSummonerRank(summoner_name)
 
     if summoner is not False:
-        print(summoner)
         if summoner_rank is not False:
-            summoner_string = f'{summoner.name} - Nível {summoner.level} - {summoner_rank.league.tier} ' \
-                              f'{summoner_rank.division} ({summoner_rank.league_points} LP)'
-            return summoner_string
+            wr = str(round(summoner_rank.wins/(summoner_rank.wins+summoner_rank.losses)*100, 2))
+            played = summoner_rank.wins+summoner_rank.losses
+            embed = discord.Embed(title=f'{summoner.name} - Nível {summoner.level}',
+                                  description=f'**{summoner_rank.league.tier} {summoner_rank.division} '
+                                              f'({summoner_rank.league_points} LP) - {wr}% WR ({played} Jogos)**\n\n'
+                                              f'**[OPGG](https://euw.op.gg/summoners/euw/{summoner_name})**',
+                                  colour=discord.Colour.blue())
+            return embed
         else:
-            summoner_string = f'{summoner.name} - Nível {summoner.level} - Unranked'
-            return summoner_string
+            embed = discord.Embed(title=f'{summoner.name} - Nível {summoner.level}',
+                                  description=f'Unranked\n\n'
+                                              f'**[OPGG](https://euw.op.gg/summoners/euw/{summoner_name})**',
+                                  colour=discord.Colour.blue())
+            return embed
     else:
-        print("tou aqui")
-        summoner_string = 'Quem? :banana:'
+        summoner_string = 'Quem? :heheboy:'
         return summoner_string
 
 
-def isLolSummonerRequest(msg):
-    if msg.startswith("!lol "):
-        return True
+def requestType(msg):
+    if msg.startswith('!lol '):
+        return 'lol_summoner'
+    elif msg.startswith('!lolgame'):
+        return 'lolgame'
     else:
         return False
 
 
+# Split game from summoner name, strip summoner name for
+# names with multiple words
+def extractNameFromMessage(user_message):
+    split_message = user_message.split(" ", 1)
+    name = split_message[1].strip()
+    return name
+
+
 def getSummonerRank(name):
     try:
-        rank = cass.get_summoner(name=name, region="EUW").league_entries.fives
+        rank = cass.get_summoner(name=name, region='EUW').league_entries.fives
         return rank
     except Exception as err:
         return False
@@ -73,9 +126,14 @@ async def on_message(message):
         return
 
     if message.channel.name == 'testing':
-        if isLolSummonerRequest(user_message):
-            await message.channel.send(getSummonerString(user_message))
+        if requestType(user_message) == 'lol_summoner':
+            summoner_name = extractNameFromMessage(user_message)
+            await message.channel.send(embed=generateSummonerString(summoner_name))
             return
-
+        elif requestType(user_message) == 'lolgame':
+            await message.channel.send('Ora espera lá um segundinho...')
+            summoner_name = extractNameFromMessage(user_message)
+            await message.channel.send(generateMatchString(summoner_name))
+            return
 
 client.run(TOKEN)
